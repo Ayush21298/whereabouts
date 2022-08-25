a55876d1582c2d66cdd8d8901eb53381972ad6c8
Ayush Patel <patel.ayush08@gmail.com>
2022-08-25 20:40:18 +0900

    Support backward compatibility with DualStack
    Unified old IP configuration paramteres like Range etc. along with new
    IPRanges to support backward compatibility.
    
    Signed-off-by: Ayush Patel <patel.ayush08@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -76,6 +76,21 @@ func LoadIPAMConfig(bytes []byte, envArgs string, extraConfigPaths ...string) (*
 		logging.Debugf("Used defaults from parsed flat file config @ %s", foundflatfile)
 	}
 
+  if n.IPAM.Range != "" {
+
+    logging.Debugf("Deprecated Range present in IPAM config. It will soon be removed in later releases. Please use IPRanges instead")
+
+    oldRange := types.RangeConfiguration {
+      Addresses: n.IPAM.Addresses,
+      OmitRanges: n.IPAM.OmitRanges,
+      Range: n.IPAM.Range,
+      RangeStart: n.IPAM.RangeStart,
+      RangeEnd: n.IPAM.RangeEnd,
+    }
+
+    n.IPAM.IPRanges = append(n.IPAM.IPRanges, oldRange)
+  }
+
 	for idx, _ := range n.IPAM.IPRanges {
 		if r := strings.SplitN(n.IPAM.IPRanges[idx].Range, "-", 2); len(r) == 2 {
 			firstip := netutils.ParseIPSloppy(r[0])
